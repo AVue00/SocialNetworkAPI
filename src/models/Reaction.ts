30da4ff1f274cf3f4c43cc237d4a11ddffbe02b8
AVue2 <Vuey00@gmail.com>
2025-04-18 10:46:46 -0400

    fixed spelling error for reactionId on line 13

--- src/models/Reaction.ts
+++ src/models/Reaction.ts
@@ -10,7 +10,7 @@ interface IReaction extends Document {
 
 const ReactionSchema = new Schema<IReaction>(
     {
-        reactionID: {
+        reactionId: {
             type: Schema.Types.ObjectId,
             default: () => new Types.ObjectId()
         },
