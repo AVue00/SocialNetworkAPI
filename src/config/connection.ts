0dcf7d8825c377cc747f1ca766c4a0c616c818f8
AVue2 <Vuey00@gmail.com>
2025-04-20 19:32:00 -0400

    Fixed spelling error in connection.ts file

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const db = async (): Promise<typeof mongoose.connection> => {
     try {
         await mongoose.connect('mongodb://127.0.0.1:27017/networkDB');
-        console.log('Databse connected');
+        console.log('Database connected');
         return mongoose.connection;
     } catch(error) {
         console.error('Database connection error:', error);
